1b00f907e8ab966ed7ff1ccc4e83ca4ebd38fed8
Константин Соколов <sokolovk055@gmail.com>
2023-04-13 00:03:34 +0300

    Добавил модель произведений и юзер

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from reviews.models import Title, User
+from .serializers import TitleReadSerializer, UsersSerializer
+from rest_framework import viewsets
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+
+class UsersViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UsersSerializer
+
+
+class TitleViewSet(ModelViewSet):
+    """
+    Получить список всех объектов. Права доступа: Доступно без токена
+    """
+    queryset = Title.objects.all()
+    serializer_class = TitleReadSerializer
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,75 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+USER = 'user'
+ADMIN = 'admin'
+MODERATOR = 'moderator'
+
+ROLE_CHOICES = [
+    (USER, USER),
+    (ADMIN, ADMIN),
+    (MODERATOR, MODERATOR),
+]
+
+
+class User(AbstractUser):
+    username = models.CharField(
+        max_length=150,
+        unique=True,
+        blank=False,
+        null=False
+    )
+    email = models.EmailField(
+        max_length=254,
+        unique=True,
+        blank=False,
+        null=False
+    )
+    role = models.CharField(
+        'роль',
+        max_length=20,
+        choices=ROLE_CHOICES,
+        default=USER,
+        blank=True
+    )
+    bio = models.TextField(
+        'биография',
+        blank=True,
+    )
+    first_name = models.CharField(
+        'имя',
+        max_length=150,
+        blank=True
+    )
+    last_name = models.CharField(
+        'фамилия',
+        max_length=150,
+        blank=True
+    )
+
+    def __str__(self):
+        return self.username
+
+
+class Title(models.Model):
+    name = models.CharField(
+        'название',
+        max_length=200,
+        db_index=True
+    )
+    year = models.IntegerField(
+        'год',
+    )
+    category = models.CharField(
+        'категория',
+        max_length=150,
+        blank=True
+    )
+    genre = models.CharField(
+        'жанр',
+        max_length=150,
+        blank=True
+    )
+
+    def __str__(self):
+        return self.name
