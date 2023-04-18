5f1ea68b14e9901179f2f7adf8556a9f0077f898
Константин Соколов <sokolovk055@gmail.com>
2023-04-18 16:44:53 +0300

    Пофиксил 3 теста

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -9,7 +9,16 @@ class IsAdminOrReadOnly(permissions.BasePermission):
 
 class IsAdminPermission(permissions.BasePermission):
     def has_permission(self, request, view):
-        return request.user.is_authenticated and request.user.is_admin
+        return (
+            request.user.is_admin
+            or request.user.is_staff
+        )
+
+    def has_object_permission(self, request, view, obj):
+        return (
+            request.user.is_admin
+            or request.user.is_staff
+        )
 
 
 class IsAuthorAdminModerOrReadOnly(permissions.BasePermission):
