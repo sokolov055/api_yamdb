1272c13e81535b20e17775ed8beff39bc3c9d5f2
Константин Соколов <sokolovk055@gmail.com>
2023-04-20 16:16:25 +0300

    Merg to master

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,6 +1,8 @@
+from django.db.models import Avg
+from api.filters import TitleFilter
 from django.db import IntegrityError
 from django.shortcuts import get_object_or_404
-from rest_framework import permissions, status, viewsets
+from rest_framework import status, viewsets
 from rest_framework.decorators import action, api_view
 from django_filters.rest_framework import DjangoFilterBackend
 from django.contrib.auth.tokens import default_token_generator
